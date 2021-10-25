17994abc30d34a911f4c5e1e7599d3f4b29c92c9
unknown <thenmazhai93@gmail.com>
2021-10-25 20:26:22 +0530

    color changed

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCZYGBg6UqDBXdf65--D6ZL-j-cW_C2mRc",
+    authDomain: "classtest-27d11.firebaseapp.com",
+    projectId: "classtest-27d11",
+    storageBucket: "classtest-27d11.appspot.com",
+    messagingSenderId: "441384539986",
+    appId: "1:441384539986:web:bdadbcf69b186a1aaccc8b"
+  };
+  
+ 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
